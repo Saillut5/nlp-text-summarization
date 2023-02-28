906ef0185317db66575f58c9c2619e32d544dc82
Silas Vance <phosubvieworsi@gmx.net>
2023-02-28 16:11:00 -0500

    feat: Introduce support for different language models

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -15,3 +15,4 @@ __all__ = [
 # Simulated change on 2023-01-05 13:10:00
 # Simulated change on 2023-01-17 15:06:00
 # Simulated change on 2023-02-01 09:19:00
+# Simulated change on 2023-02-28 16:11:00
