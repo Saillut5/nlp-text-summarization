e0b017e592a9d945d09f5d5c7da4c37c77e61a76
Silas Vance <phosubvieworsi@gmx.net>
2023-04-19 18:31:00 -0400

    refactor: Extract common NLP utilities

--- summarizer/models/abstractive.py
+++ summarizer/models/abstractive.py
@@ -124,3 +124,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-02-27 14:58:00
 # Simulated change on 2023-03-21 16:24:00
 # Simulated change on 2023-03-27 17:20:00
+# Simulated change on 2023-04-19 18:31:00
