8467ef1af4071fbd89723fd7261561f8afcf1353
Silas Vance <phosubvieworsi@gmx.net>
2023-05-29 14:14:00 -0400

    refactor: Modularize summarization models

--- summarizer/models/extractive.py
+++ summarizer/models/extractive.py
@@ -93,3 +93,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-03-28 16:58:00
 # Simulated change on 2023-04-17 17:20:00
 # Simulated change on 2023-04-25 18:36:00
+# Simulated change on 2023-05-29 14:14:00
