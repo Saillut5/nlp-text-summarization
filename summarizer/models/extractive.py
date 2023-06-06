12cfe96b28880c972f5e241869bb415b3f435dab
Silas Vance <phosubvieworsi@gmx.net>
2023-06-06 11:52:00 -0400

    docs: Document model parameters and usage

--- summarizer/models/extractive.py
+++ summarizer/models/extractive.py
@@ -95,3 +95,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-04-25 18:36:00
 # Simulated change on 2023-05-29 14:14:00
 # Simulated change on 2023-06-05 18:58:00
+# Simulated change on 2023-06-06 11:52:00
