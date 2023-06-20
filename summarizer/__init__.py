7dddb834694ad721ec129edf5f7b047291568a84
Silas Vance <phosubvieworsi@gmx.net>
2023-06-20 16:21:00 -0400

    feat: Add batch summarization functionality

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -27,3 +27,4 @@ __all__ = [
 # Simulated change on 2023-06-15 16:07:00
 # Simulated change on 2023-06-19 09:48:00
 # Simulated change on 2023-06-20 13:52:00
+# Simulated change on 2023-06-20 16:21:00
