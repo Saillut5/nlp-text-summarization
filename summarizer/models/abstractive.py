aab40461283fabce27629786109beae43d59a8d4
Silas Vance <phosubvieworsi@gmx.net>
2023-07-11 09:53:00 -0400

    test: Add integration tests for full summarization pipeline

--- summarizer/models/abstractive.py
+++ summarizer/models/abstractive.py
@@ -127,3 +127,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-04-19 18:31:00
 # Simulated change on 2023-05-02 18:54:00
 # Simulated change on 2023-07-10 17:57:00
+# Simulated change on 2023-07-11 09:53:00
