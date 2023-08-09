ee4217ccf2d0efdd7493b59b014a20be17dd9544
Silas Vance <phosubvieworsi@gmx.net>
2023-08-09 17:53:00 -0400

    fix: Handle long text inputs for summarization

--- summarizer/models/extractive.py
+++ summarizer/models/extractive.py
@@ -98,3 +98,4 @@ if __name__ == "__main__":
 # Simulated change on 2023-06-06 11:52:00
 # Simulated change on 2023-07-13 13:55:00
 # Simulated change on 2023-08-03 18:22:00
+# Simulated change on 2023-08-09 17:53:00
