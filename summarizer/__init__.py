9f8bbc19f3fe3dc3cc2f7687c42459fb34b58135
Silas Vance <phosubvieworsi@gmx.net>
2023-09-27 13:09:00 -0400

    feat: Implement ExtractiveSummarizer with TF-IDF

--- summarizer/__init__.py
+++ summarizer/__init__.py
@@ -35,3 +35,4 @@ __all__ = [
 # Simulated change on 2023-08-24 09:18:00
 # Simulated change on 2023-09-18 17:53:00
 # Simulated change on 2023-09-26 14:24:00
+# Simulated change on 2023-09-27 13:09:00
