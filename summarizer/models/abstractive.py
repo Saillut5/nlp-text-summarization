72b1e9c32990626b032fd818a6351b0a12e1609e
Silas Vance <phosubvieworsi@gmx.net>
2024-02-21 17:34:00 -0500

    feat: Introduce support for different language models

--- summarizer/models/abstractive.py
+++ summarizer/models/abstractive.py
@@ -146,3 +146,4 @@ if __name__ == "__main__":
 # Simulated change on 2024-01-15 13:01:00
 # Simulated change on 2024-01-16 16:44:00
 # Simulated change on 2024-01-26 09:26:00
+# Simulated change on 2024-02-21 17:34:00
