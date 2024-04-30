4c3ee3397ba6b9b47e1106afe6deee3364ca9cd9
Silas Vance <phosubvieworsi@gmx.net>
2024-04-30 14:46:00 -0400

    perf: Optimize summarization speed for large datasets

--- summarizer/models/extractive.py
+++ summarizer/models/extractive.py
@@ -125,3 +125,4 @@ if __name__ == "__main__":
 # Simulated change on 2024-04-19 17:32:00
 # Simulated change on 2024-04-22 18:17:00
 # Simulated change on 2024-04-25 16:09:00
+# Simulated change on 2024-04-30 14:46:00
